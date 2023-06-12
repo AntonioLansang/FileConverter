b4095a0e9cb3a83ece9b1056d5f9fdea145c02c7
Antonio Lansang <antoniolansang500@gmail.com>
2023-06-12 08:53:21 -0500

    Basic jpg to png conversion complete

--- FileConverter.py
+++ FileConverter.py
@@ -1,1 +1,15 @@
+from PIL import Image as img
+import os
 
+
+def main():
+   
+
+    Source=img.open(r'/Users/antoniolansang/Downloads/TestFolder/20230531_115032(1).jpg')
+    Source.save(r'/Users/antoniolansang/Downloads/TestFolder/test.png')
+
+
+
+
+if __name__ == '__main__':
+    main()
