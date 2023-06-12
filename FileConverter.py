92c0b12369feb2334b7c338a058ee735a135433a
Antonio Lansang <antoniolansang500@gmail.com>
2023-06-12 15:28:38 -0500

    Testing

--- FileConverter.py
+++ FileConverter.py
@@ -4,7 +4,8 @@ import os
 
 def main():
    
-
+#This is the basic logic that we want to do.
+# Now we just need to implement tkinter to make it cool
     Source=img.open(r'/Users/antoniolansang/Downloads/TestFolder/20230531_115032(1).jpg')
     Source.save(r'/Users/antoniolansang/Downloads/TestFolder/test.png')
 
