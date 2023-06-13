ee81c6c2673d5f80e18dd9e702563bbdf4a92074
Antonio Lansang <antoniolansang500@gmail.com>
2023-06-13 14:52:17 -0500

    Added another button and separated selecting video and audio file functionality

--- FileConverter.py
+++ FileConverter.py
@@ -16,16 +16,39 @@ class MyGUI:
         self.label.pack(padx=10, pady=10)
         self.root.geometry("500x500")
 
-        self.button=tk.Button(self.root, text="Open File Explorer", font=('Arial',18), command=self.SelectFile)
+        self.button=tk.Button(self.root, text="Select video file", font=('Arial',18), command=self.SelectVideoFile)
         self.button.pack(padx=10,pady=10)
+
+        self.button=tk.Button(self.root, text="Select Audio File", font=('Arial',18), command=self.SelectAudioFile)
+        self.button.pack(padx=10,pady=10)
+
+
 
 
         self.root.mainloop()
 
 #It is important that we have the word "self" in the parameters because we are in a class
-    def SelectFile(self):
+    def SelectVideoFile(self):
         filetypes = (
-            ('text files', '*.txt'),
+            ('text files', '*.mp4'),
+            ('audio files', '*.m4v'),
+            ('All files', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+
+        showinfo(
+            title='You Have selected:',
+            message=filename
+        )
+
+
+    def SelectAudioFile(self):
+        filetypes = (
+            ('text files', '*.mp3'),
             ('audio files', '*.m4a'),
             ('All files', '*.*')
         )
@@ -41,9 +64,12 @@ class MyGUI:
         )
 
 
-def main():
 
+
+
+def main():
     MyGUI()
+
 
 if __name__ == '__main__':
     main()
